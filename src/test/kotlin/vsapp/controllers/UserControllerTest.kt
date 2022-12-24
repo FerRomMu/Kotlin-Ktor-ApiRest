341189c0870c856c97fb1647e356ea575f39294a
Fernando Romero <fer.rom.mu@gmail.com>
2022-12-24 17:30:38 -0300

    Edit controllerTest to verified calls to service.

--- src/test/kotlin/vsapp/controllers/UserControllerTest.kt
+++ src/test/kotlin/vsapp/controllers/UserControllerTest.kt
@@ -2,6 +2,7 @@ package vsapp.controllers
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
 import vsapp.model.User
 import vsapp.model.dtos.LoginUserDTO
@@ -37,40 +38,46 @@ class UserControllerTest {
     }
 
     @Test
-    fun `si el usuario es valido al loguear obtengo su DTO`() {
+    fun `si el usuario es valido al loguear obtengo su DTO y hace login del servicio`() {
         val result = userController.login(LoginUserDTO("a", "a"))
         assertEquals(result, aUserDTO)
+        verify(exactly = 1) { mockService.login("a","a") }
     }
 
     @Test
-    fun `si el usuario es invalido al loguear devuelve null`() {
+    fun `si el usuario es invalido al loguear devolveria null y hace login del servicio`() {
         val result = userController.login(LoginUserDTO("a", "asd"))
         assertEquals(result, null)
+        verify(exactly = 1) { mockService.login("a","asd") }
     }
 
     @Test
-    fun `si el usuario existe al pedirlo lo recibo`() {
+    fun `si el usuario existe al pedirlo lo recibo y hace getUser del servicio`() {
         val result = userController.getUser(0L)
         assertEquals(result, aUserDTO)
+        verify(exactly = 1) { mockService.getUser(0L) }
     }
 
     @Test
-    fun `si el usuario no existe al pedirlo recibo null`() {
+    fun `si el usuario no existe al pedirlo recibiría null y hace getUser del servicio`() {
         val result = userController.getUser(999999999999999L)
         assertEquals(result, null)
+        verify(exactly = 1) { mockService.getUser(999999999999999L) }
     }
 
     @Test
-    fun `si trato de registrar un usuario valido, se registra y me lo devuelve`() {
+    fun `si trato de registrar un usuario valido, llama al signin del servicio y devuelve el dto de ese user`() {
         val newUser = userController.signUp(SignInDTO("fafafa", "1234", "fa@test.com"))
         assertEquals(newUser, aUserDTO)
+        verify(exactly = 1) { mockService.signUp("fafafa", "1234", "fa@test.com") }
     }
 
     @Test
-    fun `si trato de registrar a alguien con un mail o username invalido no se registra`() {
+    fun `si trato de registrar a alguien con un mail o username invalido devolvería null y hace un signup por cada intento`() {
         var newUser = userController.signUp(SignInDTO("a", "1234", "fa@test.com"))
         assertEquals(newUser, null)
         newUser = userController.signUp(SignInDTO("fafafa", "1234", "a@a"))
         assertEquals(newUser, null)
+        verify(exactly = 2) { mockService.signUp(any(), any(), any()) }
     }
 }
