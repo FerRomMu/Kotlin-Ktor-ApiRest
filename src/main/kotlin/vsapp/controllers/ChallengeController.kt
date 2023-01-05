1577ad897a633fe95835e4e1067fb2c6e4ac6daa
Fernando Romero <fer.rom.mu@gmail.com>
2023-01-05 16:41:54 -0300

    Add categories and result routes implementation for challenge.

--- src/main/kotlin/vsapp/controllers/ChallengeController.kt
+++ src/main/kotlin/vsapp/controllers/ChallengeController.kt
@@ -1,5 +1,16 @@
 package vsapp.controllers
 
+import vsapp.model.dtos.CategoriesDTO
+import vsapp.model.dtos.ChallengeDTO
+import vsapp.model.dtos.PartyDTO
+
 class ChallengeController {
+    fun getCategories(): CategoriesDTO {
+        TODO()
+    }
+
+    fun getChallenge(party: PartyDTO, id: Long): ChallengeDTO {
+        TODO()
+    }
 
 }
--- src/main/kotlin/vsapp/routes/ChallengeRoutes.kt
+++ src/main/kotlin/vsapp/routes/ChallengeRoutes.kt
@@ -1,9 +1,15 @@
 package vsapp.routes
 
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import vsapp.controllers.ChallengeController
+import vsapp.model.dtos.PartyDTO
 
 fun Route.challengeRoutes() {
 
@@ -11,9 +17,17 @@ fun Route.challengeRoutes() {
 
     route("challenge"){
         authenticate {
-            get("categories"){}
-            get(){}
-            put("result"){}
+            get("categories"){
+                call.respond(controller.getCategories())
+            }
+            get(){
+                val userId = call.principal<JWTPrincipal>()!!.payload.getClaim("userId").asLong()
+                val challenge = controller.getChallenge(call.receive<PartyDTO>(), userId)
+                call.respond(challenge)
+            }
+            put("result"){
+                
+            }
         }
     }
 }
