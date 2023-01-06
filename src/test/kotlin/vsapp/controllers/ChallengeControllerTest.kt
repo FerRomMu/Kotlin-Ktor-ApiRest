ae184dca58bcb511218a5948d12e9b9fb398263c
Fernando Romero <fer.rom.mu@gmail.com>
2023-01-06 16:10:04 -0300

    Add test for getChallenge on challenge controller's unit test.

--- src/test/kotlin/vsapp/controllers/ChallengeControllerTest.kt
+++ src/test/kotlin/vsapp/controllers/ChallengeControllerTest.kt
@@ -4,20 +4,32 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import vsapp.model.Category
+import vsapp.model.Challenge
 import vsapp.model.dtos.CategoriesDTO
+import vsapp.model.dtos.CategoryDTO
+import vsapp.model.dtos.ChallengeDTO
+import vsapp.model.dtos.ChallengeRequestDTO
 import vsapp.model.dtos.mapping.ChallengeMapper
 import vsapp.service.ChallengeService
 import kotlin.test.Test
 import kotlin.test.BeforeTest
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class ChallengeControllerTest {
 
     private val mockCategory1 = mockk<Category>()
     private val mockCategory2 = mockk<Category>()
+    private val mockCategoryDTO = mockk<CategoryDTO>()
     private val mockCategoriesDTO = mockk<CategoriesDTO>()
+
+    private val mockChallenge = mockk<Challenge>()
+    private val mockChallengeDTO = mockk<ChallengeDTO>()
+    private val mockRequestDTO = mockk<ChallengeRequestDTO>()
+
     private val mockService = mockk<ChallengeService>()
     private val mockMapper = mockk<ChallengeMapper>()
+
     private val controller = ChallengeController(mockMapper, mockService)
 
     @BeforeTest
@@ -37,4 +49,33 @@ class ChallengeControllerTest {
         verify(exactly = 1) { mockService.getCategories() }
     }
 
+    @Test
+    fun `getChallenge llama a getChallenge del service y devuelve un challenge`() {
+        every { mockService.getChallenge(eq(mockCategory1), eq(1L), eq(1L)) } returns mockChallenge
+        every { mockRequestDTO.category } returns mockCategoryDTO
+        every { mockRequestDTO.memberId } returns 1L
+        every { mockRequestDTO.partyId } returns 1L
+        every { mockMapper.categoryToDTO(eq(mockCategoryDTO)) } returns mockCategory1
+        every { mockMapper.challengeToDTO(eq(mockChallenge)) } returns mockChallengeDTO
+
+        val result = controller.getChallenge(mockRequestDTO, 1L)
+
+        assertEquals(result, mockChallengeDTO)
+        verify(exactly = 1) { mockService.getChallenge(any(), any(), any()) }
+    }
+
+    @Test
+    fun `getChallenge llama a getChallenge del service y devuelve null si no hay dicho miembro para ese usuario`() {
+        every { mockService.getChallenge(eq(mockCategory1), eq(1L), eq(1L)) } returns null
+        every { mockRequestDTO.category } returns mockCategoryDTO
+        every { mockRequestDTO.memberId } returns 1L
+        every { mockRequestDTO.partyId } returns 1L
+        every { mockMapper.categoryToDTO(eq(mockCategoryDTO)) } returns mockCategory1
+        every { mockMapper.challengeToDTO(isNull()) } returns null
+
+        val result = controller.getChallenge(mockRequestDTO, 1L)
+
+        assertNull(result)
+        verify(exactly = 1) { mockService.getChallenge(any(), any(), any()) }
+    }
 }
