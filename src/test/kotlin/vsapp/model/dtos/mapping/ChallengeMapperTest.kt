04d9d6ac100eafe91e646b92ab1a9ab2a34652c1
Fernando Romero <fer.rom.mu@gmail.com>
2023-01-09 22:29:51 -0300

    Create ChallengeMapperTest with test for create categoriesToDTO.

---
+++ src/test/kotlin/vsapp/model/dtos/mapping/ChallengeMapperTest.kt
@@ -0,0 +1,65 @@
+package vsapp.model.dtos.mapping
+
+import io.mockk.mockk
+import kotlin.test.Test
+import vsapp.model.Category
+import vsapp.model.Challenge
+import vsapp.model.ChallengeResult
+import vsapp.model.Member
+import vsapp.model.dtos.*
+import kotlin.test.assertEquals
+
+class ChallengeMapperTest {
+
+    private val challengerMapper = ChallengeMapper()
+
+    @Test
+    fun `test categoriesToDTO method`() {
+
+        val category = Category(1L, "fafafa", "descripción")
+        val category2 = Category(1L, "fefefe", "descripción")
+        val list = listOf(category2, category)
+        val expectedDTO = CategoriesDTO(
+            listOf(
+                challengerMapper.categoryToDTO(category2),
+                challengerMapper.categoryToDTO(category)
+            )
+        )
+
+        val result = challengerMapper.categoriesToDTO(list)
+
+        assertEquals(expectedDTO, result)
+
+    }
+
+    @Test
+    fun `test categoryToDTO method`() {
+        val category = Category(1L, "fafafa", "descripción")
+        val expectedDTO = CategoryDTO(1L, "fafafa", "descripción")
+
+        val result = challengerMapper.categoryToDTO(category)
+
+        assertEquals(expectedDTO, result)
+    }
+
+    @Test
+    fun `test categoryFromDTO method`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `test challengeToDTO method`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `test resultFromDTO method`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `test pointsToDTO method`() {
+        TODO("Not yet implemented")
+    }
+
+}
